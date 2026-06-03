80d88cf1cd2e77f4a36ee314a488b13e780574eb
Mohd Chand Miyan <mohdchandmiyan9621@gmail.com>
2026-06-03 23:12:17 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/SecurityConfiguration/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.demo.SecurityConfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                    .defaultSuccessUrl("/welcome", true)
+                    .permitAll()
+            )
+            .logout(logout -> logout.permitAll());
+
+        return http.build();
+    }
+}
